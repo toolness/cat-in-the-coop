0ee5d9161a740152cdd98361eab2c3e3a6950b32
Atul Varma <varmaa@gmail.com>
2021-01-31 11:43:18 -0500

    Give the Cooper Hewitt solid-colored walls.

--- assets/Cooper_Hewitt/import_cooper_hewitt.gd
+++ assets/Cooper_Hewitt/import_cooper_hewitt.gd
@@ -10,4 +10,16 @@ func post_import(scene):
                 print("Modifying material for " + node.name + " to be less shiny.")
                 material.params_diffuse_mode = SpatialMaterial.DIFFUSE_TOON
                 material.params_specular_mode = SpatialMaterial.SPECULAR_DISABLED
+                var tex = material.get_texture(SpatialMaterial.TEXTURE_ALBEDO)
+                if tex:
+                    var size = tex.get_size()
+                    var image = tex.get_data()
+                    image.decompress()
+                    image.lock()
+                    var color = image.get_pixel(size.x / 2, size.y / 2)
+                    image.unlock()
+                    print("Center is color " + str(color))
+                    material.set_texture(SpatialMaterial.TEXTURE_ALBEDO, null)
+                    material.albedo_color = color
+                    print("Albedo color is now " + str(material.albedo_color) + ".")
     return scene
