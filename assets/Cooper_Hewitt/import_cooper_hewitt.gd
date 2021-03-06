92d9504850f5fcd8028001cbb5ee42b2fc4aef49
Atul Varma <varmaa@gmail.com>
2021-03-05 20:58:05 -0500

    Remove irrelevant import code.

--- assets/Cooper_Hewitt/import_cooper_hewitt.gd
+++ assets/Cooper_Hewitt/import_cooper_hewitt.gd
@@ -14,9 +14,7 @@ func iterate_node(parent):
             var mesh = node.mesh
             var material = mesh.surface_get_material(0)
             if material is SpatialMaterial:
-                print("Modifying material for " + node.name + " to be less shiny.")
-                material.params_diffuse_mode = SpatialMaterial.DIFFUSE_TOON
-                material.params_specular_mode = SpatialMaterial.SPECULAR_DISABLED
+                print("Modifying material for " + node.name + ".")
                 if material.albedo_texture is StreamTexture:
                     var mat = ShaderMaterial.new()
                     mat.shader = shader
