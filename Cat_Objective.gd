b694fa77a557b486074b9043037d6898128a2d59
Atul Varma <varmaa@gmail.com>
2021-03-06 08:46:45 -0500

    Don't parent camera to viewport during development.

--- Cat_Objective.gd
+++ Cat_Objective.gd
@@ -5,3 +5,19 @@ class_name CatObjective
 onready var collision_area: Area = $Collision_Area
 onready var picture: Viewport = $Picture
 onready var cat_spawn: Spatial = $Cat_Spawn
+onready var camera: Camera = $Camera
+
+
+func _ready():
+    # During development, we have the camera as a direct descendant
+    # of the objective so it moves with it, but at runtime we
+    # re-parent it to the picture viewport so it actually renders
+    # to it. (If we have it parented to the picture viewport during
+    # development, Godot won't actually move the camera with the
+    # rest of the objective, which is annoying.)
+    var origin = camera.global_transform.origin
+    var rot = camera.global_transform.basis
+    self.remove_child(camera)
+    picture.add_child(camera)
+    camera.global_transform.origin = origin
+    camera.global_transform.basis = rot
