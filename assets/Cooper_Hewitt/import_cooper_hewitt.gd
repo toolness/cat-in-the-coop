8c6dd4be54e26c2af50edd3db08c38d7f1021a1a
Atul Varma <varmaa@gmail.com>
2021-03-05 21:53:58 -0500

    Make textures look bitmappy (#8)
    This uses a custom shader to make the textures look more bitmappy.

--- assets/Cooper_Hewitt/import_cooper_hewitt.gd
+++ assets/Cooper_Hewitt/import_cooper_hewitt.gd
@@ -1,6 +1,8 @@
 tool
 extends EditorScenePostImport
 
+var shader = preload("res://bitmappy_shader.shader")
+
 func post_import(scene):
     iterate_node(scene)
     return scene
@@ -12,7 +14,10 @@ func iterate_node(parent):
             var mesh = node.mesh
             var material = mesh.surface_get_material(0)
             if material is SpatialMaterial:
-                print("Modifying material for " + node.name + " to be less shiny.")
-                material.params_diffuse_mode = SpatialMaterial.DIFFUSE_TOON
-                material.params_specular_mode = SpatialMaterial.SPECULAR_DISABLED
+                print("Modifying material for " + node.name + ".")
+                if material.albedo_texture is StreamTexture:
+                    var mat = ShaderMaterial.new()
+                    mat.shader = shader
+                    mat.set_shader_param("tex", material.albedo_texture)
+                    mesh.surface_set_material(0, mat)
         iterate_node(node)
---
+++ bitmappy_shader.shader
@@ -0,0 +1,19 @@
+shader_type spatial;
+
+uniform sampler2D tex;
+
+const float TEX_SIZE = 256.0;
+
+void fragment() {
+	float x = UV.x;
+	float y = UV.y;
+	x = floor(x * TEX_SIZE) / TEX_SIZE;
+	y = floor(y * TEX_SIZE) / TEX_SIZE;
+	// We have to use textureLod() here, as using texture() somehow
+	// causes artifacts to appear.
+	ALBEDO = textureLod(tex, vec2(x, y), 0.0).rgb;
+}
+
+void light() {
+	DIFFUSE_LIGHT += clamp(dot(NORMAL, LIGHT), 0.0, 1.0) * (ATTENUATION*2.5) * ALBEDO;
+}
