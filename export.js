98a3f95988d544ca90361a59f21fa9330704a5bb
Atul Varma <varmaa@gmail.com>
2021-03-08 09:01:44 -0500

    Add TODOs.

--- export.js
+++ export.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const child_process = require('child_process');
 
+// TODO: support publishing to itch via e.g.:
+//   butler push dist toolness/cat-in-the-coop:html --userversion 0.0.1
+//   butler push cat-in-the-coop-osx.zip toolness/cat-in-the-coop:osx-beta --userversion 0.0.1
+//   butler push dist-windows toolness/cat-in-the-coop:windows-beta --userversion 0.0.1
+
 const DIST_DIR = path.join(__dirname, 'dist');
 
 if (!fs.existsSync(DIST_DIR)) {
